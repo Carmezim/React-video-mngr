5f717ab586fda56588b923db5f8986a86b38b3c7
Carmezim <carmezim.filho@gmail.com>
2016-11-17 00:51:13 +0200

    updating src add, edit functionalities

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
+import {loadAuthors} from './actions/authorActions';
 import {loadCourses} from './actions/courseActions';
 import routes from './routes';
 import './styles/styles.css';
@@ -11,6 +12,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
